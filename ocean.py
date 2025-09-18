2fe0ed836ed263f44ceb292666ba053e88260e6f
mbkartsandcreations246-cyber <mbkartsandcreations246@gmail.com>
2025-09-18 22:16:57 +0530

    Update ocean.py

--- ocean.py
+++ ocean.py
@@ -94,7 +94,7 @@ elif choice == "Taxonomy Explorer":
         "Chordata": {
             "Actinopterygii (Ray-finned fishes)": {
                 "Thunnus albacares (Yellowfin Tuna)": {
-                    "image": "https://upload.img.freepik.com/premium-photo/yellowfin-tuna-thunnus-albacares-swimming-blue-water-hunting-ocean-wildlife_174533-100935.jpg",
+                    "image": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Thunnus_albacares.png",
                     "info": "Yellowfin tuna is found in pelagic waters of tropical and subtropical oceans worldwide."
                 },
                 "Sardinella longiceps (Indian Oil Sardine)": {
