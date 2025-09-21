97f150c180f0eba9f3accd2c0c5c54f092c6322d
mbkartsandcreations246-cyber <mbkartsandcreations246@gmail.com>
2025-09-21 21:59:09 +0530

    Update species_data.py

--- species_data.py
+++ species_data.py
@@ -7,85 +7,81 @@ taxonomy = {
                     "Carcharhinus sorrah (Spot-tail shark)": {
                         "info": "A common reef shark found in Indo-Pacific waters.",
                         "image": "https://media.marinewise.com.au/wp-content/uploads/2021/10/24124750/Spot-Tail-Shark-Marinewise.png"
-                    },
+                    }
                 },
-            "Carcharhiniformes_2": {
                 "Scoliodonidae": {
                     "Scoliodon laticaudus (Spadenose shark)": {
                         "info": "A small shark species found in shallow coastal waters.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/6/61/Scoliodon_laticaudus.jpg"
-                        },
-                    },
-                },
+                    }
+                }
+            },
             "Myliobatiformes": {
                 "Rhinobatidae": {
                     "Rhinobatos granulatus (Granulated shovel nose ray)": {
                         "info": "A species of guitarfish found in shallow waters along India.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/3/31/Rhinobatos_granulatus.jpg"
-                        },
-                    },
-                "Dasyatidae (Stingrays)":{
-                    "Himantura gerrardi (Whitespotted whipray)":{
-                        "info": "A large stingray species found in shallow coastal waters of the Indo-Pacific. Recognizable by its white spots on a dark body and a long whip-like tail. It is benthic, feeding on crustaceans and small fish.",
+                    }
+                },
+                "Dasyatidae": {
+                    "Himantura gerrardi (Whitespotted whipray)": {
+                        "info": "A large stingray species found in shallow coastal waters of the Indo-Pacific. Recognizable by its white spots on a dark body and a long whip-like tail. It feeds on crustaceans and small fish.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/4/44/Himantura_gerrardi.jpg"
-                        },
-                    },
-                },  
-            },
+                    }
+                }
+            }
+        },
         "Actinopterygii": {
             "Clupeiformes": {
                 "Clupeidae": {
                     "Sardinella longiceps (Indian oil sardine)": {
                         "info": "A commercially important small pelagic fish in India.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/9/97/Sardinella_longiceps.jpg"
-                        },
-                "Engraulidae":{    
+                    },
                     "Stolephorus indicus (Indian anchovy)": {
                         "info": "A small schooling fish found in Indian coastal waters.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/2/2a/Stolephorus_indicus.jpg"
-                        },
-                        },
-                    },
-                },
+                    }
+                }
+            },
             "Perciformes": {
                 "Carangidae": {
                     "Rastrelliger kanagurta (Indian mackerel)": {
                         "info": "A widely consumed fast-swimming fish along Indian coasts.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/4/41/Rastrelliger_kanagurta.jpg"
-                        }
-                    },
+                    }
+                },
                 "Lutjanidae": {
                     "Lutjanus johnii (Spotted snapper)": {
                         "info": "A reef-dwelling snapper, important for local fisheries.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/5/55/Lutjanus_johnii.jpg"
-                        }
-                    },
-                },
+                    }
+                }
+            },
             "Pleuronectiformes": {
                 "Cynoglossidae": {
                     "Cynoglossus semifasciatus (Malabar sole)": {
                         "info": "A flatfish species found in the Indian Ocean.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/0/03/Cynoglossus_semifasciatus.jpg"
-                        }
-                    },
-                },
+                    }
+                }
+            },
             "Siluriformes": {
                 "Ariidae": {
                     "Osteogeneiosus militaris (Soldier catfish)": {
                         "info": "A brackish water catfish species from Indian coasts.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/5/53/Osteogeneiosus_militaris.jpg"
-                        }
                     }
-                },
+                }
+            },
             "Tetraodontiformes": {
                 "Tetraodontidae": {
                     "Chelonodon patoca (Milk-spotted puffer)": {
                         "info": "A small pufferfish species found in coastal India.",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/2/2f/Chelonodon_patoca.jpg"
-                        }
                     }
                 }
-            },
-        },
+            }
+        }
     }
 }
