249e424d98d3ea49f2025f24c3395252f5897838
mbkartsandcreations246-cyber <mbkartsandcreations246@gmail.com>
2025-09-21 12:56:53 +0530

    Update ocean.py

--- ocean.py
+++ ocean.py
@@ -65,7 +65,8 @@ elif choice == "Visualization":
         df.columns = df.columns.str.strip().str.lower().str.replace("_", " ")
         # Line plot: temperature vs species count
         if {"temperature", "region"}.issubset(df.columns):
-            fig = px.bar(df,x="region",y="temperature",title="Average Temperature by Region")
+            fig = px.bar(df,x="region",y="temperature",
+                         width=[0.3]*len(df),title="Average Temperature by Region")
             st.plotly_chart(fig, use_container_width=True)
         # Scatter: Salinity vs pH
         if {"salinity", "ph","species count"}.issubset(df.columns):
