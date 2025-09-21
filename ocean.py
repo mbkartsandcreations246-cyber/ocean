6dc4079a506e4c68801065d8494ffede7ecd7138
mbkartsandcreations246-cyber <mbkartsandcreations246@gmail.com>
2025-09-21 19:48:00 +0530

    Update ocean.py

--- ocean.py
+++ ocean.py
@@ -96,8 +96,6 @@ elif choice == "Visualization":
             hover_name="species",
             zoom=5,                   
             height=600,
-            size=[30]*len(df),
-            
         )
         fig.update_layout(mapbox_style="open-street-map")  # free base map
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
