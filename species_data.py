cd88aa30fa0c292518bf23c18d7c3598adc4f3b3
mbkartsandcreations246-cyber <mbkartsandcreations246@gmail.com>
2025-09-21 22:16:06 +0530

    Update species_data.py

--- species_data.py
+++ species_data.py
@@ -5,15 +5,15 @@ taxonomy = {
             "Carcharhiniformes": {
                 "Carcharhinidae": {
                     "Carcharhinus sorrah (Spot-tail shark)": {
-                        "info": "This small, spindle-shaped shark is found in the tropical Indo-West Pacific. It lives on continental and insular shelves, often near reefs. It feeds on bony fishes, cephalopods, and crustaceans. The species is viviparous, giving birth to live young. It is caught by small-scale commercial fisheries for food and its fins.
+                        "info": """This small, spindle-shaped shark is found in the tropical Indo-West Pacific. It lives on continental and insular shelves, often near reefs. It feeds on bony fishes, cephalopods, and crustaceans. The species is viviparous, giving birth to live young. It is caught by small-scale commercial fisheries for food and its fins.
 
-",
+""",
                         "image": "https://media.marinewise.com.au/wp-content/uploads/2021/10/24124750/Spot-Tail-Shark-Marinewise.png"
                     }
                 },
                 "Scoliodonidae": {
                     "Scoliodon laticaudus (Spadenose shark)": {
-                        "info": "A small shark with a distinctive trowel-shaped snout, it is common in the tropical Indian and Western Pacific Oceans, including the coastal and estuarine waters of the northern Indian Ocean. It is highly productive, with large litters of 6–20 pups. It is a dominant shark in fisheries in Pakistan, northern India, and Bangladesh, and is caught for human consumption.",
+                        "info": """A small shark with a distinctive trowel-shaped snout, it is common in the tropical Indian and Western Pacific Oceans, including the coastal and estuarine waters of the northern Indian Ocean. It is highly productive, with large litters of 6–20 pups. It is a dominant shark in fisheries in Pakistan, northern India, and Bangladesh, and is caught for human consumption.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/6/61/Scoliodon_laticaudus.jpg"
                     }
                 }
@@ -21,13 +21,13 @@ taxonomy = {
             "Myliobatiformes": {
                 "Rhinobatidae": {
                     "Rhinobatos granulatus (Granulated shovel nose ray)": {
-                        "info": "This guitarfish is found in the Indo-West Pacific. It lives on sandy or muddy bottoms in shallow coastal waters. It feeds on crustaceans, mollusks, and small fish. Females are ovoviviparous, giving birth to 6-28 live pups per litter.",
+                        "info": """This guitarfish is found in the Indo-West Pacific. It lives on sandy or muddy bottoms in shallow coastal waters. It feeds on crustaceans, mollusks, and small fish. Females are ovoviviparous, giving birth to 6-28 live pups per litter.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/3/31/Rhinobatos_granulatus.jpg"
                     }
                 },
                 "Dasyatidae": {
                     "Himantura gerrardi (Whitespotted whipray)": {
-                        "info": "A large stingray species found in shallow coastal waters of the Indo-Pacific. Recognizable by its white spots on a dark body and a long whip-like tail. It feeds on crustaceans and small fish.",
+                        "info": """A large stingray species found in shallow coastal waters of the Indo-Pacific. Recognizable by its white spots on a dark body and a long whip-like tail. It feeds on crustaceans and small fish.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/4/44/Himantura_gerrardi.jpg"
                     }
                 }
@@ -37,11 +37,11 @@ taxonomy = {
             "Clupeiformes": {
                 "Clupeidae": {
                     "Sardinella longiceps (Indian oil sardine)": {
-                        "info": " This is a highly migratory, pelagic-neritic species found in the northern and western Indian Ocean. It forms schools in coastal waters and feeds mainly on phytoplankton, especially diatoms. It has a lifespan of about 2.5-3 years and spawns once a year during the southwest monsoon off the western coast of India. It is a commercially important fish, marketed fresh, dried, salted, or used for fish meal.",
+                        "info": """ This is a highly migratory, pelagic-neritic species found in the northern and western Indian Ocean. It forms schools in coastal waters and feeds mainly on phytoplankton, especially diatoms. It has a lifespan of about 2.5-3 years and spawns once a year during the southwest monsoon off the western coast of India. It is a commercially important fish, marketed fresh, dried, salted, or used for fish meal.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/9/97/Sardinella_longiceps.jpg"
                     },
                     "Stolephorus indicus (Indian anchovy)": {
-                        "info": "Also known as Hardenberg's anchovy, this ray-finned fish is an oceanodromous species in the family Engraulidae. It is a common commercial fish in countries like Sri Lanka and is also used as bait in the tuna fishery.",
+                        "info": """Also known as Hardenberg's anchovy, this ray-finned fish is an oceanodromous species in the family Engraulidae. It is a common commercial fish in countries like Sri Lanka and is also used as bait in the tuna fishery.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/2/2a/Stolephorus_indicus.jpg"
                     }
                 }
@@ -49,15 +49,15 @@ taxonomy = {
             "Perciformes": {
                 "Carangidae": {
                     "Rastrelliger kanagurta (Indian mackerel)": {
-                        "info": "The Indian mackerel is an important pelagic fish widely distributed in the Indo-West Pacific. It is a planktonivorous feeder, consuming both phytoplankton (like diatoms) and zooplankton (like crustaceans). It is a shoaling species, and India contributes a significant portion of the world's production.",
+                        "info": """The Indian mackerel is an important pelagic fish widely distributed in the Indo-West Pacific. It is a planktonivorous feeder, consuming both phytoplankton (like diatoms) and zooplankton (like crustaceans). It is a shoaling species, and India contributes a significant portion of the world's production.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/4/41/Rastrelliger_kanagurta.jpg"
                     }
                 },
                 "Lutjanidae": {
                     "Lutjanus johnii (Spotted snapper)": {
-                        "info": "Also known as John's snapper, this fish is found in the Indo-West Pacific. Adults typically live around coral reefs, while juveniles are found in mangrove estuaries. It feeds on smaller fishes and benthic invertebrates such as crabs and shrimps.
+                        "info": """Also known as John's snapper, this fish is found in the Indo-West Pacific. Adults typically live around coral reefs, while juveniles are found in mangrove estuaries. It feeds on smaller fishes and benthic invertebrates such as crabs and shrimps.
 
-",
+""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/5/55/Lutjanus_johnii.jpg"
                     }
                 }
@@ -65,7 +65,7 @@ taxonomy = {
             "Pleuronectiformes": {
                 "Cynoglossidae": {
                     "Cynoglossus semifasciatus (Malabar sole)": {
-                        "info": "This species of tonguefish is found in the Indian Ocean, specifically off the coast of eastern India and Sri Lanka. It lives on muddy or sandy bottoms and feeds on benthic invertebrates. It is often caught as bycatch in demersal trawl fisheries.",
+                        "info": """This species of tonguefish is found in the Indian Ocean, specifically off the coast of eastern India and Sri Lanka. It lives on muddy or sandy bottoms and feeds on benthic invertebrates. It is often caught as bycatch in demersal trawl fisheries.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/0/03/Cynoglossus_semifasciatus.jpg"
                     }
                 }
@@ -73,7 +73,7 @@ taxonomy = {
             "Siluriformes": {
                 "Ariidae": {
                     "Osteogeneiosus militaris (Soldier catfish)": {
-                        "info": "This sea catfish is found in the Indian and western Pacific Oceans, from Pakistan to the Malay Archipelago. It inhabits marine, brackish, and freshwater environments along coasts. It can grow up to 35 cm long and is commercially caught for human consumption.",
+                        "info": """This sea catfish is found in the Indian and western Pacific Oceans, from Pakistan to the Malay Archipelago. It inhabits marine, brackish, and freshwater environments along coasts. It can grow up to 35 cm long and is commercially caught for human consumption.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/5/53/Osteogeneiosus_militaris.jpg"
                     }
                 }
@@ -81,7 +81,7 @@ taxonomy = {
             "Tetraodontiformes": {
                 "Tetraodontidae": {
                     "Chelonodon patoca (Milk-spotted puffer)": {
-                        "info": " Also known by names such as milk-spotted pufferfish, this species is native to the Indo-Pacific. It inhabits coastal waters, lagoons, estuaries, and rivers, and can be found in both brackish and fresh water, though typically not far from the sea. The species is poisonous, but is sometimes considered a delicacy in Japan.",
+                        "info": """ Also known by names such as milk-spotted pufferfish, this species is native to the Indo-Pacific. It inhabits coastal waters, lagoons, estuaries, and rivers, and can be found in both brackish and fresh water, though typically not far from the sea. The species is poisonous, but is sometimes considered a delicacy in Japan.""",
                         "image": "https://upload.wikimedia.org/wikipedia/commons/2/2f/Chelonodon_patoca.jpg"
                     }
                 }
