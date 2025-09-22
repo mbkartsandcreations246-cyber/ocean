a764df850bae7d3b6bf35cd165f7c17b1c18b738
mbkartsandcreations246-cyber <mbkartsandcreations246@gmail.com>
2025-09-22 21:03:34 +0530

    Update species_data.py

--- species_data.py
+++ species_data.py
@@ -8,7 +8,7 @@ taxonomy = {
                         "info": """This small, spindle-shaped shark is found in the tropical Indo-West Pacific. It lives on continental and insular shelves, often near reefs. It feeds on bony fishes, cephalopods, and crustaceans. The species is viviparous, giving birth to live young. It is caught by small-scale commercial fisheries for food and its fins.
 
 """,
-                        "image": "https://media.marinewise.com.au/wp-content/uploads/2021/10/24124750/Spot-Tail-Shark-Marinewise.png"
+                        "image": "images/Spot_Tail_Shark_Marinewise.png"
                     }
                 },
                 "Scoliodonidae": {
@@ -22,13 +22,13 @@ taxonomy = {
                 "Rhinobatidae": {
                     "Rhinobatos granulatus (Granulated shovel nose ray)": {
                         "info": """This guitarfish is found in the Indo-West Pacific. It lives on sandy or muddy bottoms in shallow coastal waters. It feeds on crustaceans, mollusks, and small fish. Females are ovoviviparous, giving birth to 6-28 live pups per litter.""",
-                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrMMFPVP1Cxd3_MJWPe5Y2SAoXNIX6QGBKjw&s"
+                        "image": "images/Rhinobatos_granulatus_(Granulated_shovel_nose_ray).jpg"
                     }
                 },
                 "Dasyatidae": {
                     "Himantura gerrardi (Whitespotted whipray)": {
                         "info": """A large stingray species found in shallow coastal waters of the Indo-Pacific. Recognizable by its white spots on a dark body and a long whip-like tail. It feeds on crustaceans and small fish.""",
-                        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Whipray_mandalay_bay3.jpg/250px-Whipray_mandalay_bay3.jpg"
+                        "image": "images/Himantura_gerrardi_(Whitespotted_whipray).jpg"
                     }
                 }
             }
@@ -38,11 +38,11 @@ taxonomy = {
                 "Clupeidae": {
                     "Sardinella longiceps (Indian oil sardine)": {
                         "info": """ This is a highly migratory, pelagic-neritic species found in the northern and western Indian Ocean. It forms schools in coastal waters and feeds mainly on phytoplankton, especially diatoms. It has a lifespan of about 2.5-3 years and spawns once a year during the southwest monsoon off the western coast of India. It is a commercially important fish, marketed fresh, dried, salted, or used for fish meal.""",
-                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfonaIMah3QSkeRTvwFUxsaO8q5N9Yxr7_GA&s"
+                        "image": "images/Sardinella_longiceps_(Indian_oil _sardine).jpg"
                     },
                     "Stolephorus indicus (Indian anchovy)": {
                         "info": """Also known as Hardenberg's anchovy, this ray-finned fish is an oceanodromous species in the family Engraulidae. It is a common commercial fish in countries like Sri Lanka and is also used as bait in the tuna fishery.""",
-                        "image": "https://ala-images.s3.ap-southeast-2.amazonaws.com/store/2/f/7/a/14c205c0-5723-4ae7-9dbd-192e460da7f2/thumbnail_large"
+                        "image": "images/Stolephorus_indicus_(Indian_anchovy).jpg"
                     }
                 }
             },
@@ -50,7 +50,7 @@ taxonomy = {
                 "Carangidae": {
                     "Rastrelliger kanagurta (Indian mackerel)": {
                         "info": """The Indian mackerel is an important pelagic fish widely distributed in the Indo-West Pacific. It is a planktonivorous feeder, consuming both phytoplankton (like diatoms) and zooplankton (like crustaceans). It is a shoaling species, and India contributes a significant portion of the world's production.""",
-                        "image": "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcTi3eyP4eUGRMSMcqAVcPc3TWe_tenp6ULh7lad1SH4004x2V7T5oB9vTFHU6fInnsMFg_k1CbgEf7ypGxIgFap26CZxOU8"
+                        "image": "images/indian_mackerel_silvery_rastrelligerkanagurta.png"
                     }
                 },
                 "Lutjanidae": {
@@ -58,7 +58,7 @@ taxonomy = {
                         "info": """Also known as John's snapper, this fish is found in the Indo-West Pacific. Adults typically live around coral reefs, while juveniles are found in mangrove estuaries. It feeds on smaller fishes and benthic invertebrates such as crabs and shrimps.
 
 """,
-                        "image": "https://marinebiodiversity.org.bd/wp-content/uploads/2024/01/Lutjanus-johnii_ABR-Lab.jpg"
+                        "image": "images/Lutjanus_johnii_.jpg"
                     }
                 }
             },
@@ -66,7 +66,7 @@ taxonomy = {
                 "Cynoglossidae": {
                     "Cynoglossus semifasciatus (Malabar sole)": {
                         "info": """This species of tonguefish is found in the Indian Ocean, specifically off the coast of eastern India and Sri Lanka. It lives on muddy or sandy bottoms and feeds on benthic invertebrates. It is often caught as bycatch in demersal trawl fisheries.""",
-                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3BLNKMsx7HrMJTUUiqzn-5AdNRHntOKLPqA&s"
+                        "image": "images/Cynoglossus_semifasciatus_(Malabar_sole).jpg"
                     }
                 }
             },
@@ -74,7 +74,7 @@ taxonomy = {
                 "Ariidae": {
                     "Osteogeneiosus militaris (Soldier catfish)": {
                         "info": """This sea catfish is found in the Indian and western Pacific Oceans, from Pakistan to the Malay Archipelago. It inhabits marine, brackish, and freshwater environments along coasts. It can grow up to 35 cm long and is commercially caught for human consumption.""",
-                        "image": "https://upload.wikimedia.org/wikipedia/commons/5/53/Osteogeneiosus_militaris.jpg"
+                        "image": "images/
                     }
                 }
             },
@@ -82,7 +82,7 @@ taxonomy = {
                 "Tetraodontidae": {
                     "Chelonodon patoca (Milk-spotted puffer)": {
                         "info": """ Also known by names such as milk-spotted pufferfish, this species is native to the Indo-Pacific. It inhabits coastal waters, lagoons, estuaries, and rivers, and can be found in both brackish and fresh water, though typically not far from the sea. The species is poisonous, but is sometimes considered a delicacy in Japan.""",
-                        "image": "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcSCQ5dd3tZcg1sAXQ11oS96JHXJGLYP_J2e29UE8S3VLtYUj-jyPOpgx5SlrXp2LzEx8AH2gQrRdTQSjKgqlPvya9gO1Hl1K"
+                        "image": "images/Chelonodon_patoca_(Milk_spotted_puffer).jpg"
                     }
                 }
             }
