f9e42270f88ed6a0ade6b7220bb086f7b8a8d376
mbkartsandcreations246-cyber <mbkartsandcreations246@gmail.com>
2025-09-23 20:58:57 +0530

    Update taxonomy_data.py

--- taxonomy_data.py
+++ taxonomy_data.py
@@ -7,9 +7,9 @@ taxonomy = {
                 "Carcharhinidae": {
                     "Carcharhinus sorrah (Spot-tail shark)": {
                         "info": """
-                        \n This small, spindle-shaped shark is found in the tropical Indo-West Pacific. It lives on continental and insular shelves, often near reefs.
-                        - It feeds on bony fishes, cephalopods, and crustaceans.
-                        - The species is viviparous, giving birth to live young. It is caught by small-scale commercial fisheries for food and its fins.
+                        \n* This small, spindle-shaped shark is found in the tropical Indo-West Pacific. It lives on continental and insular shelves, often near reefs.
+                        \n* It feeds on bony fishes, cephalopods, and crustaceans.
+                        \n* The species is viviparous, giving birth to live young. It is caught by small-scale commercial fisheries for food and its fins.
 
 """,
                         "image": "images/Spot_Tail_Shark_Marinewise.png"
